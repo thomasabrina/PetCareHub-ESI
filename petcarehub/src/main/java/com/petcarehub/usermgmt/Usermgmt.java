126449ae38263a5cec4652ff618ee100bd7dae75
Xinjian Zhang <117434829+Xinjian-Zhang@users.noreply.github.com>
2024-03-13 13:51:46 +0200

    Usermgmt Framework

---
+++ petcarehub/src/main/java/com/petcarehub/usermgmt/Usermgmt.java
@@ -0,0 +1,14 @@
+package com.petcarehub.usermgmt;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
+public class Usermgmt {
+
+        private int uid;
+        private String hashpwd;
+        private String name;
+        private String email;
+        private String addr;
+
+
+}
---
+++ petcarehub/src/main/java/com/petcarehub/usermgmt/UsermgmtController.java
@@ -0,0 +1,8 @@
+package com.petcarehub.usermgmt;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsermgmtController {
+
+
+}
