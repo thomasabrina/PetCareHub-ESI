fdb5b2e790c1e51476e5c1ba0c85a0e2ca10de9f
Xinjian Zhang <117434829+Xinjian-Zhang@users.noreply.github.com>
2024-03-13 13:22:01 +0200

    Initial Commit

---
+++ petcarehub/src/main/java/com/petcarehub/petcarehub/PetcarehubApplication.java
@@ -0,0 +1,13 @@
+package com.petcarehub.petcarehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcarehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcarehubApplication.class, args);
+	}
+
+}
---
+++ petcarehub/src/test/java/com/petcarehub/petcarehub/PetcarehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petcarehub.petcarehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcarehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
